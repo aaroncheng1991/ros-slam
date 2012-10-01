45f2d5376ba4d1f037b9154a563fa588a963f340
vandelaerjoren@gmail.com <vandelaerjoren@gmail.com@3e5579ca-4359-0f65-3523-a7f052a689ea>
2012-10-01 07:08:17 +0000

    added random walk assignment in the project folder as a starting point

---
+++ trunk/Project/turtlebotSlam/build/CMakeFiles/random_walk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/random_walk.dir/src/random_walk.o"
+  "../bin/random_walk.pdb"
+  "../bin/random_walk"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/random_walk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
