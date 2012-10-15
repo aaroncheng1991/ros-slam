209b2c6fc1f8edc0fe83ed20bbc170c0829e7c61
driesderydt <driesderydt@hotmail.com>
2012-10-15 18:23:55 +0000

    lol

--- Assignments/particle_filter/src/ParticleFilter.cpp
+++ Assignments/particle_filter/src/ParticleFilter.cpp
@@ -8,7 +8,6 @@
 #include <boost/random/normal_distribution.hpp>
 
 
-
  /**  
   * Just place all particles on a line along x=y. This should be
   * replaced with something more sensible, like drawing particles
@@ -19,13 +18,13 @@
   void MyLocaliser::initialisePF( const geometry_msgs::PoseWithCovarianceStamped& initialpose )
   {
 	  boost::mt19937 rng; 
-	  boost::normal_distribution<> xDistr(initialpose.pose.pose.position.x,2.0);
+      boost::normal_distribution<> xDistr(initialpose.pose.pose.position.x,1.0);
 	  boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varX(rng, xDistr);
 
-	  boost::normal_distribution<> yDistr(initialpose.pose.pose.position.y,2.0);
+      boost::normal_distribution<> yDistr(initialpose.pose.pose.position.y,1.0);
 	  boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varY(rng, yDistr);
 
-	  boost::normal_distribution<> oDistr(tf::getYaw(initialpose.pose.pose.orientation),2.0);
+      boost::normal_distribution<> oDistr(tf::getYaw(initialpose.pose.pose.orientation),1.0);
 	  boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varO(rng, oDistr);
 
     for (unsigned int i = 0; i < particleCloud.poses.size(); ++i)
@@ -46,12 +45,26 @@
    */
   void MyLocaliser::applyMotionModel( double deltaX, double deltaY, double deltaT )
   {
-    if (deltaX > 0 or deltaY > 0)
+      boost::mt19937 rng;
+      boost::normal_distribution<> xDistr(deltaX,0.001);
+      boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varX(rng, xDistr);
+      boost::normal_distribution<> yDistr(deltaY,0.001);
+      boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varY(rng, yDistr);
+
+      boost::normal_distribution<> oDistr(deltaT,0.001);
+      boost::variate_generator<boost::mt19937&,boost::normal_distribution<> > varO(rng, oDistr);
+
+
+    if (deltaX > 0 or deltaY > 0 or deltaT > 0){
       ROS_DEBUG( "applying odometry: %f %f %f", deltaX, deltaY, deltaT );
     for (unsigned int i = 0; i < particleCloud.poses.size(); ++i)
     {
-      particleCloud.poses[i].position.x += deltaX;
-      particleCloud.poses[i].position.y += deltaY;      
+        particleCloud.poses[i].position.x += varX();
+        particleCloud.poses[i].position.y += varY();
+
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(tf::getYaw(particleCloud.poses[i].orientation)+varO());
+        particleCloud.poses[i].orientation = odom_quat;
+    }
     }
   }
 
@@ -64,6 +77,12 @@
    */
   void MyLocaliser::applySensorModel( const sensor_msgs::LaserScan& scan )
   {
+      double eR = 0;
+      for(unsigned int k = 0; k < scan.ranges.size(); ++k){
+          eR += scan.ranges[k];
+      }
+      eR /= scan.ranges.size();
+
     /* This method is the beginning of an implementation of a beam
      * sensor model */  
     for (unsigned int i = 0; i < particleCloud.poses.size(); ++i)
@@ -84,17 +103,32 @@
         continue;
       }
 
+      double eS = 0;
+      for(unsigned int k = 0; k < simulatedScan->ranges.size(); ++k){
+          eS += simulatedScan->ranges[k];
+      }
+      eS /= simulatedScan->ranges.size();
+
       /* Now we have the actual scan, and a simulated version ---
        * i.e., how a scan would look if the robot were at the pose
        * that particle i says it is in. So now we should evaluate how
        * likely this pose is; i.e., the actual sensor model. */
-      
-      //   if (i == 0)
-      //   {
-      //     for (unsigned int k = 0; k < simulatedScan->ranges.size(); ++k)
-      //       ROS_INFO_STREAM(simulatedScan->ranges[k]);
-      //     std::cerr << "\n\n";
-      //   }
+
+      double dist = 0, maxD = 0;
+
+      for(unsigned int k = 0; k < simulatedScan->ranges.size(); ++k){
+          double temp = abs((simulatedScan->ranges[k]-eS)* (scan.ranges[k]-eR));
+          maxD = maxD < temp ? temp : maxD;
+          dist += temp;
+      }
+
+      distances[i] = (distances[i] * 0.9) + (dist/10);
+         //if (i == 0)
+         //{
+         //  for (unsigned int k = 0; k < simulatedScan->ranges.size(); ++k)
+         //    ROS_INFO_STREAM(simulatedScan->ranges[k]);
+         //  std::cerr << "\n\n";
+         //}
     }
   }
 
@@ -115,7 +149,19 @@
    */
   geometry_msgs::PoseWithCovariance MyLocaliser::updatePose()
   {
-    this->estimatedPose.pose.pose = particleCloud.poses[1];
+
+
+
+    double lowestDistance = DBL_MAX;
+    int bestGuess=0;
+    for(unsigned int i=0;i<sizeD;++i){
+        if(distances[i] < lowestDistance){
+            bestGuess=i;
+            lowestDistance = distances[i];
+        }
+    }
+
+    this->estimatedPose.pose.pose = particleCloud.poses[bestGuess];
     //TODO: also update the covariance
     return this->estimatedPose.pose;
   }
