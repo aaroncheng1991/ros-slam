07a2881862759cb89616232cc89cd8d7491fdb1b
felix.remmel@gmail.com <felix.remmel@gmail.com@3e5579ca-4359-0f65-3523-a7f052a689ea>
2012-11-27 14:14:38 +0000

    visualization stuff

---
+++ trunk/Project/visualization/include/visualizeParticles.h
@@ -0,0 +1,9 @@
+#ifndef VISUALIZEPARTICLES_H
+#define VISUALIZEPARTICLES_H
+#include <vector>
+#include "../../fastSlam/src/FastSLAMAlgorithm.h"
+namespace visualization {
+void visualizeParticles(std::vector<fslam::Particle> particles);
+
+}
+#endif
