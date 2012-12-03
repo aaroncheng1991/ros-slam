caba6ee6734e79b87b351fb00177775015867553
vandelaerjoren@gmail.com <vandelaerjoren@gmail.com@3e5579ca-4359-0f65-3523-a7f052a689ea>
2012-12-03 12:25:51 +0000

    added Eigen lib to graphslam

---
+++ trunk/Project/graph_slam/src/gSLAM.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+#include <Eigen/Dense>
+class fSLAM{
+
+	protected:
+		Matrix<int, 30, 30> omega; //has to be dynamical later
+
+		void initialize(){
+		ROSERROR("testerdetest");
+		}
+
+}
