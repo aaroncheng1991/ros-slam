e01d62f85004e4e514bcd0d89671c9087c1a913b
vandelaerjoren@gmail.com <vandelaerjoren@gmail.com@3e5579ca-4359-0f65-3523-a7f052a689ea>
2012-12-04 09:27:28 +0000

    some changes in gSLAM.cpp

---
+++ trunk/Project/graph_slam/build/CMakeFiles/graph_slam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/graph_slam.dir/src/gSLAM.o"
+  "../bin/graph_slam.pdb"
+  "../bin/graph_slam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/graph_slam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
