1e3ca5a18321b78b8aca5b2480d3bd1a1323f0aa
vandelaerjoren <vandelaerjoren@gmail.com>
2012-12-04 09:27:28 +0000

    some changes in gSLAM.cpp

--- Project/graph_slam/src/gSLAM.cpp
+++ Project/graph_slam/src/gSLAM.cpp
@@ -1,8 +1,28 @@
 #include "ros/ros.h"
 #include <Eigen/Dense>
-class fSLAM{
+class gSLAM{
 
-	protected:
-        Eigen::Matrix2d test;
+	public:
+        Eigen::Matrix2d test(3,3);
 
+				void testmethod(){	
+					ROS_ERROR("testerdetest %f", test.coef(1,1);
+				}
+
+				void run() {
+				 	ros::Rate rate(1);
+				 	while(ros::ok()) {
+					 	ros::spinOnce();
+					  rate.sleep();
+				 	}
+			 	}
 };
+
+int main(int argc, char **argv){ 
+	ros::init(argc, argv, "graphSlam");  // Initiate new ROS node named "fastSlam"
+	ros::NodeHandle n;
+	gSLAM gslam;
+	//fslam::FastSLAMAlgorithm fslam(n);      // Create new fSLAM object
+	gslam.testmethod();
+	gslam.run();
+}
